4379da9e3f9f2af69539eeedd8c26c59aecab2ee
jean-seb-2106 <jean.sebban@insee.fr>
2025-09-05 09:02:36 +0000

    ajout des données sur les connexions

--- data/pour_generer_data.sql
+++ data/pour_generer_data.sql
@@ -1,7 +1,11 @@
-#nombre de connexions uniques mensuelles
+#génération d'un CSV avec le nombre de connexions uniques mensuelles
 SELECT
     COUNT(DISTINCT l.userid) AS nombre_connexions_uniques,
     DATE_FORMAT(FROM_UNIXTIME(l.timecreated), '%Y-%m') AS mois
+INTO OUTFILE 'C:/Users/Public/Downloads/connexions_uniques.csv'
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
 FROM
     mdl_logstore_standard_log l
 WHERE
