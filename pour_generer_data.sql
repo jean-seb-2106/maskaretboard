52465f38adb20c1ce1b19a2d5bebde0978f70912
jean-seb-2106 <jean.sebban@insee.fr>
2025-09-05 07:10:05 +0000

    création d'un fichier sql pour générer les requetes permettant de créer la bdd

---
+++ pour_generer_data.sql
@@ -0,0 +1,11 @@
+#nombre de connexions uniques mensuelles
+SELECT
+    COUNT(DISTINCT l.userid) AS nombre_connexions_uniques,
+    DATE_FORMAT(FROM_UNIXTIME(l.timecreated), '%Y-%m') AS mois
+FROM
+    mdl_logstore_standard_log l
+WHERE
+    l.eventname LIKE '%user_loggedin%'
+    AND FROM_UNIXTIME(l.timecreated) BETWEEN '2025-01-01' AND '2025-12-30 23:59:59'
+GROUP BY
+    DATE_FORMAT(FROM_UNIXTIME(l.timecreated), '%Y-%m');
